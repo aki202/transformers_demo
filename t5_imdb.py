52a55907c7f5b5e8d52f8de7a10a1138967953f9
aki202 <okashimo23@gmail.com>
2020-10-03 03:47:04 +0900

    exclude T5FinerTuner class

--- t5_imdb.py
+++ t5_imdb.py
@@ -17,9 +17,10 @@ from nltk.tokenize import sent_tokenize
 import pandas as pd
 import numpy as np
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 import pytorch_lightning as pl
 
+from t5.fine_tuner import T5FineTuner
 
 from transformers import (
     AdamW,
@@ -36,120 +37,6 @@ def set_seed(seed):
         torch.cuda.manual_seed_all(seed)
 
 set_seed(42)
-
-# %%
-class T5FineTuner(pl.LightningModule):
-    def __init__(self, hparams):
-        super(T5FineTuner, self).__init__()
-        self.hparams = hparams
-
-        self.model = T5ForConditionalGeneration.from_pretrained(hparams.model_name_or_path)
-        self.tokenizer = T5Tokenizer.from_pretrained(hparams.tokenizer_name_or_path)
-
-    def is_logger(self):
-        return self.trainer.proc_rank <= 0
-
-    def forward(
-        self, input_ids, attention_mask=None, decoder_input_ids=None,
-        decoder_attention_mask=None, lm_labels=None
-    ):
-        return self.model(
-            input_ids,
-            attention_mask=attention_mask,
-            decoder_input_ids=decoder_input_ids,
-            decoder_attention_mask=decoder_attention_mask,
-            lm_labels=lm_labels
-        )
-
-    def _step(self, batch):
-        lm_labels = batch["target_ids"]
-        lm_labels[lm_labels[:, :] == self.tokenizer.pad_token_id] = -100
-
-        outputs = self(
-            input_ids=batch["source_ids"],
-            attention_mask=batch["source_mask"],
-            lm_labels=lm_labels,
-            decoder_attention_mask=batch['target_mask']
-        )
-
-        loss = outputs[0]
-
-        return loss
-
-    def training_step(self, batch,  batch_idx):
-        loss = self._step(batch)
-
-        tensorboard_logs = {"train_loss": loss}
-        return {"loss": loss, "log": tensorboard_logs}
-
-    def training_epoch_end(self, outputs):
-        avg_train_loss = torch.stack([x["loss"] for x in outputs]).mean()
-        tensorboard_logs = { "avg_train_loss": avg_train_loss }
-        return {
-            "avg_train_loss": avg_train_loss,
-            "log": tensorboard_logs,
-            "progress_bar": tensorboard_logs
-        }
-
-    def validation_step(self, batch, batch_idx):
-        loss = self._step(batch)
-        return {"val_loss": loss}
-
-    def validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
-        tensorboard_logs = {"val_loss": avg_loss}
-        return {"avg_val_loss": avg_loss, "log": tensorboard_logs, 'progress_bar': tensorboard_logs}
-
-    def configure_optimizers(self):
-        "Prepare optimizer and schedule (linear warmup and decay)"
-
-        model = self.model
-        no_decay = ["bias", "LayerNorm.weight"]
-        optimizer_grouped_parameters = [
-            {
-                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
-                "weight_decay": self.hparams.weight_decay
-            },
-            {
-                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
-                "weight_decay": 0.0,
-            },
-        ]
-        optimizer = AdamW(optimizer_grouped_parameters, lr=self.hparams.learning_rate, eps=self.hparams.adam_epsilon)
-        self.opt = optimizer
-        return [optimizer]
-
-    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_idx,
-        second_order_closure=None, using_native_amp=None):
-        if self.trainer.use_tpu:
-            xm.optimizer_step(optimizer)
-        else:
-            optimizer.step()
-        optimizer.zero_grad()
-        self.lr_scheduler.step()
-
-    def get_tqdm_dict(self):
-        tqdm_dict = {"loss": "{:.3f}".format(self.trainer.avg_loss), "lr": self.lr_scheduler.get_last_lr()[-1]}
-
-        return tqdm_dict
-
-    def train_dataloader(self):
-        train_dataset = get_dataset(tokenizer=self.tokenizer, type_path="train", args=self.hparams)
-        dataloader = DataLoader(train_dataset, batch_size=self.hparams.train_batch_size, drop_last=True, shuffle=True, num_workers=4)
-        t_total = (
-            (len(dataloader.dataset) // (self.hparams.train_batch_size * max(1, self.hparams.n_gpu)))
-            // self.hparams.gradient_accumulation_steps
-            * float(self.hparams.num_train_epochs)
-        )
-        scheduler = get_linear_schedule_with_warmup(
-            self.opt, num_warmup_steps=self.hparams.warmup_steps, num_training_steps=t_total
-        )
-        self.lr_scheduler = scheduler
-        return dataloader
-
-    def val_dataloader(self):
-        val_dataset = get_dataset(tokenizer=self.tokenizer, type_path="val", args=self.hparams)
-        return DataLoader(val_dataset, batch_size=self.hparams.eval_batch_size, num_workers=4)
 
 # %%
 logger = logging.getLogger(__name__)
@@ -189,13 +76,13 @@ args_dict = dict(
     weight_decay=0.0,
     adam_epsilon=1e-8,
     warmup_steps=0,
-    train_batch_size=2,
-    eval_batch_size=2,
-    num_train_epochs=2,
-    gradient_accumulation_steps=4,
+    train_batch_size=1,
+    eval_batch_size=1,
+    num_train_epochs=1,
+    gradient_accumulation_steps=1,
     n_gpu= 1 if torch.cuda.is_available() else 0,
     early_stop_callback=False,
-    fp_16=False, # if you want to enable 16-bit training then install apex and set this to true
+    fp_16=True, # if you want to enable 16-bit training then install apex and set this to true
     opt_level='O1', # you can find out more on optimisation levels here https://nvidia.github.io/apex/amp.html#opt-levels-and-properties
     max_grad_norm=1.0, # if you enable 16-bit training then set this to a sensible value, 0.5 is a good default
     seed=42,
@@ -271,18 +158,26 @@ class ImdbDataset(Dataset):
             self.targets.append(tokenized_targets)
 
 # %%
+'''
 dataset = ImdbDataset(tokenizer, 'data/aclImdb', 'val', max_len=512)
 print(len(dataset))
-# %%
+
 data = dataset[28]
 print(tokenizer.decode(data['source_ids']))
 print(tokenizer.decode(data['target_ids']))
+'''
+
+# %%
+def get_dataset(tokenizer, type_path, args):
+    return ImdbDataset(tokenizer=tokenizer, data_dir=args.data_dir,
+        type_path=type_path,  max_len=args.max_seq_length)
 
 # %%
 args_dict.update({
     'data_dir': 'data/aclImdb',
     'output_dir': 'results/t5_imdb_sentiment',
-    'num_train_epochs':2
+    'num_train_epochs': 1,
+    'get_dataset': get_dataset,
 })
 args = argparse.Namespace(**args_dict)
 
@@ -307,11 +202,6 @@ train_params = dict(
 )
 
 # %%
-def get_dataset(tokenizer, type_path, args):
-    return ImdbDataset(tokenizer=tokenizer, data_dir=args.data_dir,
-        type_path=type_path,  max_len=args.max_seq_length)
-
-# %%
 model = T5FineTuner(args)
 
 # %%
@@ -321,3 +211,6 @@ trainer = pl.Trainer(**train_params)
 trainer.fit(model)
 
 # %%
+# save the model this way so next time you can load it using T5ForConditionalGeneration.from_pretrained
+model.model.save_pretrained('t5_base_imdb_sentiment')
+model.tokenizer.save_pretrained('t5_base_imdb_sentiment')
