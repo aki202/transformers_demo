7f34d1d5cb7a5b8cd65e34fea8efff79d0c2532e
aki202 <okashimo23@gmail.com>
2020-10-04 09:52:59 +0900

    sql-to-en trainer

---
+++ t5_sql_to_en/dataset.py
@@ -0,0 +1,83 @@
+# %%
+import glob
+import os
+import json
+import re
+from torch.utils.data import Dataset
+
+# %%
+class SQLDataset(Dataset):
+    def __init__(self, tokenizer, type_path = 'train'):
+        self.tokenizer = tokenizer
+        self.type_path = type_path
+        self.inputs = []
+        self.targets = []
+
+        self._build()
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, index):
+        source_ids = self.inputs[index]["input_ids"].squeeze()
+        target_ids = self.targets[index]["input_ids"].squeeze()
+
+        source_mask = self.inputs[index]["attention_mask"].squeeze() # might need to squeeze
+        target_mask = self.targets[index]["attention_mask"].squeeze() # might need to squeeze
+
+        return {
+            "source_ids": source_ids,
+            "source_mask": source_mask,
+            "target_ids": target_ids,
+            "target_mask": target_mask
+        }
+
+    def _build_from_file(self, filename):
+        spider_json = json.load(open('./data/spider/{}.json'.format(filename)))
+
+        for datum in spider_json:
+            source = 'translate SQL to English: {} </s>'.format(datum['query'])
+            target = '{} </s>'.format(datum['question'])
+
+            # tokenize inputs
+            tokenized_inputs = self.tokenizer.batch_encode_plus(
+                [source], max_length=512, pad_to_max_length=True,
+                return_tensors="pt", truncation=True
+            )
+            # tokenize targets
+            tokenized_targets = self.tokenizer.batch_encode_plus(
+                [target], max_length=512, pad_to_max_length=True,
+                return_tensors="pt", truncation=True
+            )
+            #print('x:{},\ty:{},\t{}\t{}'.format(x, y, source, target))
+
+            self.inputs.append(tokenized_inputs)
+            self.targets.append(tokenized_targets)
+
+    def _build(self):
+        if self.type_path == 'train':
+            self._build_from_file('train_spider')
+            self._build_from_file('train_others')
+        elif self.type_path == 'val':
+            self._build_from_file('dev')
+        else: raise 'Invalid type_path ({})'.format(self.type_path)
+
+# %%
+if __name__ == '__main__':
+    from transformers import T5Tokenizer
+    tokenizer = T5Tokenizer.from_pretrained('t5-base')
+# %%
+    dataset = SQLDataset(tokenizer, type_path='dev')
+# %%
+    print('len={}'.format(len(dataset)))
+    data = dataset[10]
+    print(data)
+    for _id in data['source_ids']:
+        id = _id.item()
+        token = tokenizer.decode(id)
+        print('{}({}), '.format(token, id), end='')
+    print('')
+    print("'{}'".format(tokenizer.decode(data['source_ids'])))
+    print("'{}'".format(tokenizer.decode(data['target_ids'])))
+
+# %%
---
+++ t5_sql_to_en/train.py
@@ -0,0 +1,71 @@
+# %%
+import argparse
+import nltk
+nltk.download('punkt')
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import pytorch_lightning as pl
+from transformers import T5Tokenizer
+
+from t5.fine_tuner import T5FineTuner
+from t5.default_args import default_args
+from t5_sql_to_en.dataset import SQLDataset
+from common.util import set_seed
+
+# %%
+set_seed(42)
+args_dict = default_args()
+
+# %%
+tokenizer = T5Tokenizer.from_pretrained('t5-base')
+
+# %%
+def get_dataset(tokenizer, type_path, args):
+    return SQLDataset(tokenizer=tokenizer, type_path=type_path)
+
+# %%
+args_dict.update({
+    'output_dir': 'results/t5_sql_to_en',
+    'train_batch_size': 8,
+    'eval_batch_size': 8,
+    'num_train_epochs': 1,
+    'get_dataset': get_dataset,
+    'max_seq_length': 512,
+})
+args = argparse.Namespace(**args_dict)
+
+checkpoint_callback = pl.callbacks.ModelCheckpoint(
+    filepath=args.output_dir,
+    prefix="t5_sql_to_en",
+    monitor="val_loss",
+    mode="min",
+    save_top_k=5
+)
+
+train_params = dict(
+    accumulate_grad_batches=args.gradient_accumulation_steps,
+    gpus=args.n_gpu,
+    max_epochs=args.num_train_epochs,
+    early_stop_callback=False,
+    precision= 16 if args.fp_16 else 32,
+    amp_level=args.opt_level,
+    gradient_clip_val=args.max_grad_norm,
+    checkpoint_callback=checkpoint_callback,
+)
+
+# %%
+model = T5FineTuner(args)
+
+# %%
+trainer = pl.Trainer(**train_params)
+
+# %%
+trainer.fit(model)
+
+# %%
+# save the model this way so next time you can load it using T5ForConditionalGeneration.from_pretrained
+model.model.save_pretrained('save/t5_sql_to_en')
+model.tokenizer.save_pretrained('save/t5_sql_to_en')
+
+# %%
